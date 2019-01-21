0fc30d119b8ee3ddd322d7ad8e468248746f0a7f
Ivan Christian <ivan_christian@mymail.sutd.edu.sg>
2019-01-21 11:51:43 +0800

    Original Commit

---
+++ android/src/com/asus/platformer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.asus.platformer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.asus.platformer.DBplatformer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DBplatformer(), config);
+	}
+}
---
+++ desktop/src/com/asus/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asus.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asus.platformer.DBplatformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DBplatformer(), config);
+	}
+}
